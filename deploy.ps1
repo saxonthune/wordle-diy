a2061be01443a838702b94aced2bf4c03505ae4c
Saxon Thune <43625690+saxonthune@users.noreply.github.com>
2025-03-23 12:08:03 -0400

    deploy fix

--- deploy.ps1
+++ deploy.ps1
@@ -3,15 +3,23 @@ param (
 )
 
 Write-Host "[deploy.ps1] Cleaning and building output directory"
-$productionUrls = Get-Content ./deploy.production.json | ConvertFrom-Json
+$productionUrls = Get-Content ./deployment.production.json | ConvertFrom-Json
 cd ./wordle-diy
+Rename-Item -Path .\.env.local -NewName .\.envfoo.local -ErrorAction Stop
 Remove-Item -Recurse -Force ./out/*
 npm run build
+if ($LASTEXITCODE -ne 0) {
+    Rename-Item -Path .\.envfoo.local -NewName .\.env.local -ErrorAction SilentlyContinue 
+    cd ..
+    Write-Host "[deploy.ps1] Build failed. Exiting." -ForegroundColor Red
+    exit $LASTEXITCODE
+}
+Rename-Item -Path .\.envfoo.local -NewName .\.env.local -ErrorAction SilentlyContinue 
 cd ..
 # build
 Write-Host "[deploy.ps1] uploading to S3"
 if ($Clean) {
-    aws s3 rm $($productionUrls.s3BucketArn) --recursive
+    aws s3 rm $($productionUrls.s3BucketArn + "/wordle/") --recursive
 }
 aws s3 sync --delete ./wordle-diy/out/ $($productionUrls.s3BucketArn) 
-Write-Host "[deploy.ps1] Done. $($productionUrls.s3BucketArn)"
+Write-Host "[deploy.ps1] Done. $($productionUrls.siteUrl)"
